5ce4c4b96bf49525d38b3ed4536293e66d1aab7a
tiwarirs <radhikatiwari1708@gmail.com>
2015-12-05 12:28:14 +0530

    Update HelloSWT14.java

--- HelloSWT14.java
+++ HelloSWT14.java
@@ -1,5 +1,7 @@
 class HelloSWT14 {
     public static void main(String[] args) {
         System.out.println("Hello SWT-14!!");
+        System.out.println("Welcome to java Programming!!");
+	System.out.println("Java is fast, secure, and reliable. From laptops to datacenters, game consoles to scientific supercomputers, cell phones to the Internet, Java is everywhere! ");
     }
 }
